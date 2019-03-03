552a9218cdd409d0f7d8547a906d771e57552be8
tanyasznnk <46744710+tanyasznnk@users.noreply.github.com>
2019-03-03 16:25:07 +0300

    Add files via upload

---
+++ цг.bash
@@ -0,0 +1,208 @@
+<TEI> 
+    <teiHeader>  
+        <fileDesc>
+            <titleStmt>
+               <title>Елисей, или раздраженный Вакх</title>
+            </titleStmt>
+            <respStmt>
+                <persName>Татьяна Сизоненко</persName>
+                <resp>создание TEI документа, выделение в тексте прямой речи/ персоналий/мест</resp>
+            </respStmt>
+         </fileDesc>
+         <profileDesc>     
+             <particDesc>
+				<listPerson type="fictional">
+					<person xml:id="a">
+                         <persName>ямщик</persName>
+                    </person>
+				</listPerson>
+                <listPerson type="myphological">
+					<person xml:id="b">
+                         <persName>Вакх</persName>
+					</person>
+					<person xml:id="c">
+                         <persName>Ермий</persName>
+					</person>
+					<person xml:id="d">
+                         <persName>Зевес</persName>
+					</person>
+                    <person xml:id="e">
+                         <persName>супруга Зевеса</persName>
+					</person>
+					<person xml:id="f">
+                         <persName>Феб</persName>
+					</person>
+					<person xml:id="g">
+                         <persName>Плутон</persName>
+					</person>
+					<person xml:id="h">
+                         <persName>Вулкан</persName>
+                    </person>
+					<person xml:id="i">
+                         <persName>Венера</persName>
+					</person>
+					<person xml:id="j">
+                         <persName>Купидо</persName>
+                    </person>
+					<person xml:id="k">
+                         <persName>Марс</persName>
+					</person>
+					<person xml:id="l">
+                         <persName>Геркулес</persName>
+                    </person>
+					<person xml:id="m">
+                         <persName>Цибела</persName>
+                    </person>
+					<person xml:id="n">
+                         <persName>Минерва</persName>
+                    </person>
+					<person xml:id="o">
+                         <persName>Нептун</persName>
+                    </person>
+					<person xml:id="p">
+                         <persName>Беллона</persName>
+                    </person>
+					<person xml:id="q">
+                         <persName>Аполлон</persName>
+                    </person>
+					<person xml:id="r">
+                         <persName>Гомер</persName>
+                    </person>
+					<person xml:id="s">
+                         <persName>Вергилий</persName>
+                    </person>
+					<person xml:id="t">
+                         <persName>Пиндар</persName>
+                    </person>
+                 </listPerson>
+              </particDesc>
+          </profileDesc>
+    </teiHeader>
+    <text>    
+<person xml:id="a"><persName>Его</persName></person> уже теперь в полицию хмельнова
+Ведут, или свели, где цепь<person xml:id="a"><persName>ему</persName></person>готова.
+<person xml:id="d"><persName>Ты</persName></person> можешь,<person xml:id="d"><persName>отче мой</persName></person>, сие предупредить
+И<person xml:id="a"><persName>друга моего</persName></person>от кошек свободить:
+<person xml:id="b"><persName><person xml:id="d"><persName>я</persName></person>/persName></person> знаю, на <person xml:id="a"><persName>него</persName></person> там все вознегодуют
+И кошками <person xml:id="a"><persName>ему</persName></person> всю репицу отдуют.
+Полиция уже мне стала дорога,
+И в ней-то точного имею <person xml:id="d"><persName>я</persName></person>врага:
+Она всех забияк и пьяниц ненавидит
+И более <person xml:id="b"><persName>меня</persName></person>, чем <person xml:id="d"><persName>ты</persName></person>, еще обидит;
+От ней-то к пьянству все пресечены пути.
+Помилуй,<person xml:id="d"><persName>отче мой</persName></person>, вступись и защити!»
+Тогда <person xml:id="d"><persName>Зевес</persName></person> к себе <person xml:id="c"><persName>Ермия</persName></person> призывает,
+Призвав, и тако <person xml:id="d"><persName>он</persName></person> <person xml:id="c"><persName>ему</persName></person> повелевает:
+«Послушайся <person xml:id="b"><persName>меня</persName></person>, <person xml:id="c"><persName>возлюбленный мой сын</persName></person>!
+<person xml:id="c"><persName>Ты</persName></person> знаешь сам, что мной рожден не <person xml:id="c"><persName>ты</persName></person> один:
+Сераль побольше <person xml:id="d"><persName>я</persName></person> султанского имею,
+И ежели теперь похвастаться <person xml:id="d"><persName>я</persName></person>смею,
+От непрестанныя забавы в прежни дни
+Побольше всех богов имею <person xml:id="d"><persName>я</persName> родни, —
+Итак, не должен ли о детях <person xml:id="d"><persName>я</persName></person> печися?
+Сему ты у меня, <person xml:id="c"><persName>Ермиюшка</persName></person>, учися,
+Не чудно, что <person xml:id="d"><persName>я</persName></person> вам столь многим есмь <person xml:id="d"><persName>отец</persName></person>,
+Хитро, что мой поднесь не баливал хрестец».
+Се так разоврался <person xml:id="d"><persName>отец</persName></person> бессмертных оный
+И на́врал бы еще <person xml:id="d"><persName>он</persName></person> слов сих миллионы,
+Когда бы тут его <person xml:id="e"><persName>супруга</persName></person> не была;
+Сия из-под бровей взор косо возвела
+И тем перервала его пустые речи,
+Каких бы <person xml:id="d"><persName>он</persName></person> наклал <person xml:id="c"><persName>Ермию</persName></person> полны плечи,
+Отяготя сего разумного посла,
+И сделал бы его похожим на осла.
+Но вдруг что заврался, <person xml:id="d"><persName>он</persName></person> сам то ощущает
+И, пустословие оставя, так вещает:
+«Послушен будь, <person xml:id="c"><persName>Ермий</persName></person>, приказу моему,
+Возможно всё сие проворству твоему.
+Услуги мне твои давно уже известны;
+Оставь <person xml:id="c"><persName>ты</persName></person> сей же час селения небесны
+И слову моему со тщанием внемли,
+Ступай и на пути нимало не дремли,
+Неси скорее всем бессмертным повеленье,
+Скажи, что есть на то мое благоволенье:
+Едва покажется заря на небеса
+И станет озлащать и горы и леса,
+Доколе <person xml:id="f"><persName>Феб</persName></person> с одра Фетидина не вспрянет,
+Да на <placeName>Олимп<placeName> ко мне бессмертных сонм предстанет,
+А если кто из них хоть мало укоснит,
+Тот будет обращен воронкою в зенит,
+А попросту сказать, повешу вверх ногами,
+И будет он висеть как шут между богами,
+Не со́рвется вовек, кто б ни был как удал,
+Но <person xml:id="d"><persName>я</persName></person> еще не весь приказ тебе мой дал.
+Коль будет всё сие исполнено <person xml:id="c"><persName>тобою</persName></person>,
+Потщися <person xml:id="c"><persName>ты</persName></person> потом помочь тому герою,
+О коем <person xml:id="b"><persName>Вакх</persName></person> меня с покорностью просил,
+Ступай и покажи своих <person xml:id="c"><persName>ты</persName></person> опыт сил;
+А сей герой <person xml:id="a"><persName>ямщик</persName></person>, который за буянство
+Сведен в полицию и посаже́н за пьянство,
+И если <person xml:id="c"><persName>ты</persName></person> его оттоль не свободишь,
+Так сам <person xml:id="c"><persName>ты</persName></person> у меня в остроге посидишь».
+Тогда <person xml:id="c"><persName>Ермий</persName></person> приказ Зевесов строгий внемлет;
+<person xml:id="c"><persName>Он</persName></person>, крылья привязав, посольский жезл приемлет,
+Спускается на низ с превыспренних кругов,
+Летит и ищет всех, как гончий пес, богов,
+Находит их с трудом в странах вселенной разных,
+И всех находит <person xml:id="c"><persName>он</persName></person> богов тогда не праздных:
+<person xml:id="g"><persName>Плутон</persName></person> по мертвеце с жрецами пировал,
+<person xml:id="h"><persName>Вулкан</persName></person> на <placeName>Устюжне</placeName> пивной котел ковал,
+И знать, что помышлял <person xml:id="h"><persName>он</persName></person> к празднику о браге.
+<person xml:id="i"><persName>Жена его</persName></person> была у жен честных в ватаге,
+Которые собой прельщают всех людей;
+<person xml:id="j"><persName>Купидо</persName></person> на часах стоял у лебедей;
+<person xml:id="k"><persName>Марс</persName></person> с нею был тогда, а <person xml:id="l"><persName>Геркулес</persName></person> от скуки
+Играл с робятами клюкою длинной в суки;
+<person xml:id="m"><persName>Цибела</persName></person> старая во многих там избах
+Загадывала всем о счастье на бобах;
+<person xml:id="n"><persName>Минерва</persName></person>, может быть то было для игрушки,
+Точила девушкам на кружево коклюшки;
+<person xml:id="o"><persName>Нептун</persName></person>, с предлинною своею бородой,
+Трезубцем, иль, сказать яснее, острогой,
+Хотя не свойственно угрюмому толь мужу,
+Мутил от солнышка растаявшую лужу
+И преужасные в ней волны воздымал
+До тех пор, что свой весь трезубец изломал,
+Чему все малые робята хохотали,
+Снежками в <person xml:id="o"><persName>старика</persName></person> без милости метали;
+<person xml:id="o"><persName>Сей бог</persName></person> ребяческих игрушек не стерпел,
+Озлобился на них и гневом закипел,
+Хотел из них тотчас повытаскать все души;
+Но их отцы, вступясь, ему нагрели уши,
+И взашей, и в бока толкали <person xml:id="o"><persName>вод царя</persName></person>,
+При всяком так ему ударе говоря:
+«Не прогневись, что так <person xml:id="o"><persName>ты</persName></person> принят неучтиво.
+Ты встарь бывал в чести, а ныне <person xml:id="o"><persName>ты</persName></person> не в диво;
+Мы благодатию господней крещены
+И больше пращуров своих просвещены,
+Не станем бога чтить в таком, как <person xml:id="o"><persName>ты</persName></person>, <person xml:id="o"><persName>болване</persName></person>».
+Так православные кричали все крестьяне.
+<person xml:id="c"><persName>Ермий</persName></person>, приметя то, скорее прочь пошел,
+Немного погодя других богов нашел:
+Гоняла кубари на льду бичом <person xml:id="p"><persName>Беллона</persName></person>,
+Не в самой праздности нашел и <person xml:id="q"><persName>Аполлона</persName></person>,
+Во упражнении и сей пречудном был:
+<person xml:id="q"><persName>Он</persName></person>, у крестьян дрова тогда рубил,
+И, высунув язык, как пес уставши, рея,
+Удары повторял в подобие хорея,
+А иногда и ямб и дактиль выходил;
+Кругом его собор писачек разных был.
+Сии, не знаю что, между собой ворчали,
+Так, знать, они его удары примечали,
+И, выслушавши все удары топора,
+Пошли всвояси все, как будто мастера;
+По возвращении ж своем они оттоле
+Гордились, будто бы учились в <placeName>Спасской школе</placeName>:
+Не зная, каковой в каких стихах размер,
+Иной из них возмнил, что русский он <person xml:id="r"><persName>Гомер</persName></person>,
+Другой тогда себя с <person xml:id="s"><persName>Вергилием</persName></person> равняет,
+Когда еще почти он грамоте не знает;
+А третий прославлял толико всем свой дар
+И почитал себя не меньше как <person xml:id="t"><persName>Пиндар</persName></person>.
+Но то не мудрено, что так они болтали,
+Лишь только мудрено, что их стихи читали,
+Стихи, которые не стоят ничего
+У знающих, кроме презренья одного;
+
+    </text>
+</TEI>
